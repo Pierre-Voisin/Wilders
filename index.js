cacea5601e093f1dcd5e73c1fb73284c70806864
Pierre-Voisin <voisin.pierre87@gmail.com>
2024-03-19 12:14:45 +0100

    modifpierre

---
+++ index.js
@@ -0,0 +1,9 @@
+const firstName = "Yann";
+const secondname = "Anne";
+const  treename = "Antoine";
+const  fortyname = "Johane";
+
+import chalk from 'chalk';
+
+console.log(chalk.red(firstName), chalk.blue(secondname) ,chalk.green(treename), chalk.yellow(fortyname));
+
